a9af728868e39680c373bc979c74253100dbb0f3
ParadiseDuo <>
2021-03-05 17:34:02 +0800

    add testcase

--- lib/Android/013.py
+++ lib/Android/013.py
@@ -11,26 +11,26 @@ INFO = '检测App是否存在数据库文件任意读写风险'
 class DBCheck(Base):
     def scan(self):
         strline = cmdString('grep -r "Landroid/content/Context;->openOrCreateDatabase" ' + self.appPath)
-        arr = os.popen(strline).readlines()
+        arrs = os.popen(strline).readlines()
         results = []
-        for item in arr:
+        for item in arrs:
             if '.smali:' in item:
                 path = item.split(':')[0]
                 with open(path, 'r') as f:
                     lines = f.readlines()
                     lines.reverse()
-                    count = len(arr)
+                    count = len(lines)
                     name = getFileName(path)
                     for i in range(0, count):
-                        line = arr[i]
+                        line = lines[i]
                         if 'Landroid/content/Context;->openOrCreateDatabase' in line:
                             v = line.split(',')[2]
                             for j in range(i, count):
-                                ll = arr[j]
+                                ll = lines[j]
                                 if 'const/4' in ll and v in ll:
                                     value = ll.strip().split(' ')[-1]
                                     if value != '0x0':
-                                        result = name + ' : ' + str(count - i) + '\n'
+                                        result = name + ' : ' + str(count - i)
                                         if result not in results:
                                             results.append(result)
                                     break
