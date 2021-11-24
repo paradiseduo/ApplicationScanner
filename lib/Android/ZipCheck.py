3522a92b190209fe91507bbc7591ceb4c1ebb0d9
svg onload=alert(1) <14846965+paradiseduo@users.noreply.github.com>
2021-11-24 18:19:20 +0800

    fix issue #3

--- lib/Android/ZipCheck.py
+++ lib/Android/ZipCheck.py
@@ -39,6 +39,31 @@ class ZipCheck(Base):
                                     if result not in results:
                                         results.append(result)
                                 break
+                lines.reverse()
+                lineNume = len(lines)
+                index = 0
+                for line in lines:
+                    index += 1
+                    lineNume -= 1
+                    isExp = True
+                    if 'Ljava/util/zip/ZipEntry;->getName()Ljava/lang/String' in line:
+                        for i in range(index, count):
+                            p = lines[i]
+                            if 'Ljava/lang/String;->contains' in p:
+                                start = p.find("{") + 1
+                                end = p.find("}")
+                                v = p[start:end].split(', ')[-1]
+                                for j in range(i, count):
+                                    pp = lines[j]
+                                    if v in pp and 'const-string' in pp and '../' in pp:
+                                        isExp = False
+                                    if '.method' in pp:
+                                        if not isExp:
+                                            # 因为smali文件最后一行为空，而第一行不为空，因此倒叙后算index要加1才跟正序的index一致
+                                            result = name + ' : ' + str(lineNume+1)
+                                            if result in results:
+                                                results.remove(result)
+                                        break
         Info(key=self.__class__, title=TITLE, level=LEVEL, info=INFO, result='\n'.join(results)).description()
 
 
